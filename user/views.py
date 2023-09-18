b559b2220c8f9c8a5172d5b561b3d230999c9cf6
ctalv <122413805+ctalv@users.noreply.github.com>
2023-09-18 18:38:57 -0500

    post request is working

--- user/views.py
+++ user/views.py
@@ -10,10 +10,10 @@ from .forms import NewUserForm, EditUserForm
 # Django template = React views
 
 # display form
-def home(request):
-    users = User.objects.all()
-    form = NewUserForm()
-    return render(request, 'home.html', {'form': form, 'users':users})
+# def home(request):
+#     users = User.objects.all()
+#     form = NewUserForm()
+#     return render(request, 'home.html', {'form': form, 'users':users})
 
 # populates form with user info to be edited and added to database
 # def edit(request, user_id):
@@ -30,13 +30,21 @@ def home(request):
 # populates form with user info to be edited and added to database
 def edit(request, user_id):
     user = User.objects.get(pk=user_id)
-    print(user.first_name)
     form = EditUserForm(instance=user)
-    print(form)
+    print(form, "yes 1")
     if request.method == 'POST':
         form = EditUserForm(request.POST)
         if form.is_valid():
-            print(form)
+            print(form, "yes")
+            # first_name = form.cleaned_data['first_name']
+            # middle_initial = form.cleaned_data['middle_initial']
+            # last_name = form.cleaned_data['last_name']
+            # suffix = form.cleaned_data['suffix']
+            # full_name = form.cleaned_data['full_name']
+            # email = form.cleaned_data['email']
+            # gender = form.cleaned_data['gender']
+
+            form.cleaned_data
             form.save()
 
     return render(request, 'user.html', {'form': form, 'user':user})
@@ -44,16 +52,22 @@ def edit(request, user_id):
 # saves new user data
 def post(request):
     users = User.objects.all()
+    form = NewUserForm()
+
     
-    # print(form)
     if request.method == 'POST':
         form = NewUserForm(request.POST)
+
+        print(form)
         if form.is_valid():
+            print(form)
+
             form.save()
-        # text = form.cleaned_data['post'] # checks security
-        # print(text)
+
             form = NewUserForm()
-            return HttpResponse('Saved to database.')
+        
+            
+            
 
 
     return render(request, 'home.html', {'form': form, 'users':users})
